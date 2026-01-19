7e6bb9406d7b983a4b02ca5d4e796e204ab255d0
willfynch <william.finzy@protonmail.com>
2026-01-19 17:34:11 +0100

    feat: add new images and update section card styles for improved layout and responsiveness

--- src/app/home/application/components/section-card/section-card.ts
+++ src/app/home/application/components/section-card/section-card.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, input, ViewEncapsulation } from '@angular/core';
 import { RouterLink } from "@angular/router";
 
 @Component({
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -6,6 +6,7 @@ import { SectionCard } from '../../home/application/components/section-card/sect
   selector: 'app-home',
   imports: [HeroSectionComponent, SectionCard],
   templateUrl: './home.html',
+  styleUrl: './home.scss'
 })
 export class Home {
   sections: { title: string; path: string; image: string; description: string, appearingPicture: string }[] = [
@@ -14,7 +15,7 @@ export class Home {
       path: 'livres',
       image: 'https://images.pexels.com/photos/694740/pexels-photo-694740.jpeg',
       description:
-        'Découvrez notre catalogue de livres auto-édités par des auteurs indépendants francophones ',
+        'Découvrez notre catalogue de livres auto-édités par des auteurs indépendants francophones',
       appearingPicture: 'images/home/sections/library.webp',
     },
     {
@@ -22,14 +23,15 @@ export class Home {
       path: 'blog',
       image: 'images/hero-images/blog.webp',
       description:
-        'Actualités et conseils pour les auteurs auto-édités et les lecteurs passionnés ',
-      appearingPicture: 'images/home/sections/library.webp',
+        'Actualités et conseils pour les auteurs auto-édités et les lecteurs passionnés',
+      appearingPicture: 'images/home/sections/blog.webp',
     },
-    // {
-    //   title: 'Le blog',
-    //   path: 'blog',
-    //   image: 'images/hero-images/blog.webp',
-    //   description: 'Actualités et conseils pour les auteurs auto-édités et les lecteurs passionnés '
-    // }
+    {
+      title: 'Proposer mon livre',
+      path: 'proposer-mon-livre',
+      image: 'images/home/sections/submit_cover.webp',
+      description: 'Remplissez le formulaire pour proposer votre livre',
+      appearingPicture: 'images/home/sections/submit_hover.webp'
+    }
   ];
 }
